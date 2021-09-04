e559b46d5c5afea0907cd5ce816ef72cf290851a
Vishal Patel <vishal.patel@sidewayseight.co.uk>
2021-09-04 18:36:54 +0100

    Added CD for Heroku deploy, and some small edits to app

--- app.py
+++ app.py
@@ -8,3 +8,5 @@ from vcfunded.lib import get_recent_funded, get_by_category
 st.write(get_recent_funded())
 
 st.write(get_by_category())
+
+st.write("If you're a developer you can access more from the related API, which contains 30K+ funded companies (and growing), and more metrics:" , "https://vc-funded-api.herokuapp.com/")
